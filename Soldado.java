2b8169d4a823fd0f890bb19cc8ecebc8b2f01dc4
rcompanocca <ronicompanocca@gmail.com>
2023-11-09 10:14:32 -0500

    ejercicios

---
+++ Soldado.java
@@ -0,0 +1,61 @@
+// RONI COMPANOCCA CHECCO
+// CUI: 20210558
+// LABORATORIO 13
+// FUNDAMENTOS DE PROGRAMACION - LABORATORIO
+public class Soldado {
+	private String nombre;
+	private int fila;
+	private int columna;
+	private int puntos;
+	private String column;
+	
+	public Soldado() {
+		nombre = "";
+		fila = 0;
+		columna = 0;
+		puntos = 0;
+		column = "";
+	}
+
+	// METODOS MUTADORES
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+
+	public void setColumna(int columna) {
+		this.columna = columna;
+	}
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+	
+	public void setColumn(String column) {
+		this.column = column;
+	}
+
+	// METODOS ACCESORES
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	public int getColumna() {
+		return columna;
+	}
+
+	public int getPuntos() {
+		return puntos;
+	}
+	
+	public String getColumn() {
+		return column;
+	}
+}
